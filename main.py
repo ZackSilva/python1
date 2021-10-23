fd37a9d24b064bd994a97350c8b4037acc8866e2
ZackSilva <zsilva112501@gmail.com>
2021-10-23 02:32:51 -0400

    Updated RandomModuleFeature branch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-if __name__ = 'main':
+if __name__ == '__main__':
     import random
-    # This function leverages the random module to generate random numbers from 0 to 10.
+    # This function leverages the random module to generate random numbers from 1 to 10.
     def demonstrate_module():
         print(random.randrange(1, 10))
+
+    demonstrate_module()
