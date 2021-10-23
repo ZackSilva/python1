26a8361045c174cfe7f2d3d7ee55033dc8ec7156
ZackSilva <zsilva112501@gmail.com>
2021-10-23 02:31:08 -0400

    Updated WebbrowserModuleFeature branch

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ if __name__ == '__main__':
     # This function leverages the webbrowser module to allow for web surfing via Python.
     def demonstrate_module():
         webbrowser.open_new("linkedin.com/in/zackary-silva/")
+
+    demonstrate_module()
