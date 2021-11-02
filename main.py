94181277b32791a2a76c421b43912b79c4670d48
ZackSilva <zsilva112501@gmail.com>
2021-11-02 01:35:52 -0400

    Updated strings2 branch (again)

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
            if split_input[i][:1] == 'a' or split_input[i][:1] == 'e' or split_input[i][:1] == 'i' or split_input[i][:1] == 'o' or split_input[i][:1] == 'u':
                print(f"{split_input[i]}yay")
            else:
-               print(split_input)
+               print(f"{split_input[i][1:]}{split_input[i][0]}ay")
 
    piglatinize()
