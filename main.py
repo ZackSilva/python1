70d64b22e66bef805338c0a19177b3c919f95720
ZackSilva <zsilva112501@gmail.com>
2021-11-02 21:46:08 -0400

    Created strings3 branch

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 if __name__ == '__main__':
-   user_input = input("Let's piglatinize that sentence, bucko!")
-
-   def piglatinize():
-       split_input = user_input.split()
-       for i in range(len(split_input)):
-           if split_input[i][:1] == 'a' or split_input[i][:1] == 'e' or split_input[i][:1] == 'i' or split_input[i][:1] == 'o' or split_input[i][:1] == 'u':
-               print(f"{split_input[i]}yay")
-           else:
-               print(split_input)
-
-   piglatinize()
+   user_input = input("Please type a sentence with an apostrophe.")
+   def split_print_sentence():
+       split_result = user_input.split()
+       for i in (split_result):
+           print(i)
+   split_print_sentence()
