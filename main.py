a567e7489695100cd72f5773c00ea32a23edf16b
ZackSilva <zsilva112501@gmail.com>
2021-11-06 11:55:43 -0400

    Created regex_demo branch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 if __name__ == '__main__':
-   user_input = input("Please type a sentence with an apostrophe.")
-   def split_print_sentence():
-       split_result = user_input.split()
-       for i in (split_result):
-           print(i)
-   split_print_sentence()
+    import re
+    import os
+    txt = "Windows"
+    print(re.search(os.uname(sysname), txt))
